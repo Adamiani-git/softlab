b0fe7b3be45ac56e284797a5f60a75d7d7dcb791
Adamiani-git <adamianismail@gmail.com>
2021-12-20 14:02:14 +0400

    13.1

--- 13.1.js
+++ 13.1.js
@@ -4,8 +4,8 @@ function reverse(num) {
 
     while(num != 0){
         lastDigit = num % 10;
-    rev = rev * 10 + lastDigit;
-    num = Math.floor(num/10);
+        rev = rev * 10 + lastDigit;
+        num = Math.floor(num/10);
     }
 
     console.log(rev);
