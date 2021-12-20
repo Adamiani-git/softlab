d509ff45e6121deff44b8126a1b1bd3562b77f39
Adamiani-git <adamianismail@gmail.com>
2021-12-20 15:53:22 +0400

    000

--- 06.js
+++ 06.js
@@ -1,4 +1,4 @@
 const numbers = [-2, 0, 6, 3, -11, 4, -5, 10, 11, -1, 7, 9];
 let mult =1;
-numbers.filter(n => n != 0).map(n => Math.abs(n)).reduce((acc, n) => mult = acc * n);
+numbers.filter(n => n !== 0).map(n => Math.abs(n)).reduce((acc, n) => mult = acc * n);
 console.log(mult);
