166b0307b1420b9045d67862bdf32d6cc875f0bd
Adamiani-git <adamianismail@gmail.com>
2022-01-15 22:06:58 +0400

    timer

--- src/Components/PicList.js
+++ src/Components/PicList.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import PicModal from './PicModal';
 
 function PicList(props) {
 
     const [picList, setpicList] = useState()
     const [loadind, setloadind] = useState(true)
+
+    const [state, setstate] = useState({
+        title: '',
+        picUrl: ''
+    })
 
     const getPicList = async () => {
         const res = await axios.get('https://jsonplaceholder.typicode.com/photos?albumId=1')
@@ -17,8 +23,13 @@ function PicList(props) {
     }, [])
 
     const openModal = (t, p) => {
-        console.log(t + " - " + p);
+        setstate({
+            title: t,
+            picUrl: p
+        })
+
     }
+
     if (loadind) {
         return (
             <div className="spinner-border text-primary" role="status">
@@ -37,13 +48,13 @@ function PicList(props) {
             {
                 picList.map(p => (
                     <div key={p.id} className="col-2 m-1">
-                        <img src={p.thumbnailUrl} alt='Pic' title={p.title} className='thumb' onClick={() => openModal(p.title, p.url)} />
+                        <img src={p.thumbnailUrl} alt='Pic' title={p.title} className='thumb' onClick={() => openModal(p.title, p.url)} data-bs-toggle="modal" data-bs-target="#picModal" />
                     </div>
 
 
                 ))
             }
-
+            <PicModal t={state.title} p={state.picUrl} />
         </div>
     );
 }
---
+++ src/Components/PicModal.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function PicModal({ t, p }) {
+    return (
+        <div className="modal fade" id="picModal" tabIndex="-1" aria-labelledby="picModalLabel" aria-hidden="true">
+            <div className="modal-dialog modal-lg">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="picModalLabel">{t}</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body ">
+                        <div className='text-center'>
+                            <img src={p} alt='Pic' className='image-fluid' />
+                        </div>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        {/* <button type="button" className="btn btn-primary">Save changes</button> */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default PicModal;
